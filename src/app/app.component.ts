08f4c159ddb3fed83ee243d638013093d9c5e2ab
ajay <ajay@ajays-MBP-2.fios-router.home>
2020-09-29 00:30:22 -0400

    Code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,29 @@
-import { AfterViewInit, ChangeDetectorRef, AfterContentChecked, Component, ElementRef, OnInit, ViewChild, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BeepService } from './beep.service';
 import Quagga from 'quagga';
 import { Article } from './article';
 import { ShoppingCart } from './shopping-cart';
 import { ApiService } from './api.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy {
 
   errorMessage: string;
-
+  destroy$ = new Subject<void>();
   private catalogue: Article[] = [];
 
   private lastScannedCodeDate: number;
 
   constructor(
-              private beepService: BeepService,
-              private api: ApiService,
-              public shoppingCart: ShoppingCart) {
+    private beepService: BeepService,
+    private api: ApiService,
+    public shoppingCart: ShoppingCart) {
   }
 
   ngOnInit(): void {
@@ -30,26 +32,28 @@ export class AppComponent implements OnInit{
       return;
     }
 
-    this.api.get('assets/db.json').subscribe((res: any) => {
-      this.catalogue  = res;
-    });
+    this.api.get('assets/db.json')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: any) => {
+        this.catalogue = res;
+      });
 
     Quagga.init({
-        inputStream: {
-          constraints: {
-            facingMode: 'environment'
-          },
-          area: { // defines rectangle of the detection/localization area
-            top: '40%',    // top offset
-            right: '0%',  // right offset
-            left: '0%',   // left offset
-            bottom: '40%'  // bottom offset
-          },
+      inputStream: {
+        constraints: {
+          facingMode: 'environment'
         },
-        decoder: {
-          readers: ['ean_reader']
+        area: { // defines rectangle of the detection/localization area
+          top: '40%',    // top offset
+          right: '0%',  // right offset
+          left: '0%',   // left offset
+          bottom: '40%'  // bottom offset
         },
       },
+      decoder: {
+        readers: ['ean_reader']
+      },
+    },
       (err) => {
         if (err) {
           this.errorMessage = `QuaggaJS could not be initialized, err: ${err}`;
@@ -74,16 +78,20 @@ export class AppComponent implements OnInit{
     const article = this.catalogue.find(a => a.ean === code);
     if (!article) {
       alert('Product not found');
-    }else{
-        this.shoppingCart.addArticle(article);
-        this.lastScannedCodeDate = now;
-        this.beepService.beep();
+    } else {
+      this.shoppingCart.addArticle(article);
+      this.lastScannedCodeDate = now;
+      this.beepService.beep();
     }
 
   }
 
-  onClick(){
+  onClick() {
     window.print();
+  }
+
+  ngOnDestroy(){
+    this.destroy$.next();
   }
 
 }
--- src/app/shopping-cart-item/shopping-cart-item.component.ts
+++ src/app/shopping-cart-item/shopping-cart-item.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ShoppingCart } from '../shopping-cart';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shopping-cart-item',
   templateUrl: './shopping-cart-item.component.html',
   styleUrls: ['./shopping-cart-item.component.scss']
 })
-export class ShoppingCartItemComponent implements OnInit {
+export class ShoppingCartItemComponent implements OnInit, OnDestroy {
+
+  shoppingCartItems: any;
+  totalPrice: any;
+  destroy$ = new Subject<void>();
 
   constructor(public shoppingCart: ShoppingCart) {
   }
 
-  shoppingCartItems: any = new Map();
-  totalPrice: any;
-
   ngOnInit(): void {
-    this.shoppingCart.getArticles().subscribe((res) => {
-      setTimeout(() => {
+    this.shoppingCart.getArticles()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res) => {
         this.shoppingCartItems = res;
         this.totalPrice = this.getTotalPrice();
       });
-    });
   }
   isEmpty(): boolean {
     return this.shoppingCartItems.size === 0;
@@ -32,6 +35,9 @@ export class ShoppingCartItemComponent implements OnInit {
       total += entry[0].price * entry[1];
     }
     return total;
+  }
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 
 }
